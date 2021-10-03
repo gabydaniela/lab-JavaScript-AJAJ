bdadfefd94c2fedac24f82ddac75924fb715613c
gabydaniela <gabydaniela17@gmail.com>
2021-10-03 13:09:31 -0600

    Prueba

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+const p = document.getElementById('select-provincias');
+const c = document.getElementById('select-cantones');
+
+const clearSelect = () => {
+	const options = subasunto.querySelectorAll('option:not([value=""])');
+	options.forEach(option => c.removeChild(option));
+  };
+  
+  const fillSelect = (p, json) => {
+	json.forEach(item => {
+	  const option = document.createElement('option');
+	  option.value = item.id;
+	  option.text = item.nombre;
+	  option.innerText = item[item.nombre];
+	  c.appendChild(option);
+	});
+	c.disabled = false;
+  };
+  
+  p.addEventListener('change', () => {
+	clearSelect();
+	c.disabled = true;
+	const value = p.value;
+	if (value) {
+		fetch('cantones.php', { method: 'POST', body: JSON.stringify({id: id}) })
+              .then( response => response.json() )
+              .then( json => fillSelect(value, json));
+	}
+  });
+
 $(function(){
 	function cargarProvincias(){
 		var data="";
